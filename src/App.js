d1dccc96acc1f7cc87c2247dfbd0c7edfcd8345e
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-06 02:37:03 +0600

    added home & shared in pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-        
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
