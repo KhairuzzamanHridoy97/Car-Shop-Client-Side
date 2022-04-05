e4de2395fffb87d4334d86e07f79e390180f6219
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-06 03:36:05 +0600

    added product in products

---
+++ src/pages/Home/Products/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = ({ourCar}) => {
+    const {title}= ourCar ; 
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Product;
--- src/pages/Home/Products/Products/Products.js
+++ src/pages/Home/Products/Products/Products.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import Product from '../Product/Product';
 
 const Products = () => {
+    const [ourCars,setOurCars] = useState([]);
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res=>res.json())
+        .then(data=>setOurCars(data))
+    })
     return (
         <div>
             <h2 className='my-5 display-4 text-bold fw-bold text-danger'>Our Cars</h2>
+            <Container>
+            <Row xs={1} md={3} className="g-4">
+
+                {
+                    ourCars.slice(0,6).map(
+                        ourCar=> <Product 
+                        ourCar={ourCar}>
+
+                        </Product>
+                    )
+                }
+            </Row>
+            </Container>
         </div>
     );
 };
