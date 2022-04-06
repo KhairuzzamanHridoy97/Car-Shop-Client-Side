5db4a519bb1f75733b08caa214518ae5d8da05fd
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-06 14:46:55 +0600

    added Cars in pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './pages/Home/Home/Home';
+import Cars from './pages/Cars/Cars/Cars';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
             </Route>
             <Route path='/home'>
               <Home></Home>
+            </Route>
+            <Route path='/cars'>
+              <Cars></Cars>
             </Route>
           </Switch>
         </Router>
---
+++ src/pages/Cars/Cars/Cars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Cars = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h3>Purchase  Your Car</h3>
+        </div>
+    );
+};
+
+export default Cars;
