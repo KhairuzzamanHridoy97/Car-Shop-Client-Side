b87669f41208fb24c9d69fd6912e175b1eb60823
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-12 23:52:35 +0600

    import useAuth in navig

--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import useAuth from '../../../hooks/useAuth';
+import { Button } from '@mui/material';
 
 const Navigation = () => {
+    const {user,logOut} = useAuth();
     return (
        <>
             <Navbar bg="primary" variant="dark" expand="lg">
@@ -13,7 +16,13 @@ const Navigation = () => {
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to='/home' >Home</Nav.Link>
                         <Nav.Link as={Link} to='/cars' >Cars</Nav.Link>
-                        <Nav.Link as={Link} to='/login' >Login</Nav.Link>
+                        {
+                            user?.email?
+                            <Button onClick={logOut} color='primary' style={{my:1}}  >LOGOUT</Button>
+                            :
+                            <Nav.Link as={Link} to='/login' >Login</Nav.Link>
+
+                        }
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
