26fb78e8a225032fa7ed1a564fc0c19491135321
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-13 23:11:33 +0600

    added upcomingCars in home

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
 import Products from '../Products/Products/Products';
+import UpcomingCars from '../UpcomingCar/UpcomingCars/UpcomingCars';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Navigation></Navigation>
             <Banner></Banner>
             <Products></Products>
+            <UpcomingCars></UpcomingCars>
             <Footer></Footer>
         </div>
     );
---
+++ src/pages/Home/UpcomingCar/UpcomingCars/UpcomingCars.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpcomingCars = () => {
+    return (
+        <div>
+            <h2 className='text-primary my-5'>Upcoming Cars</h2>
+        </div>
+    );
+};
+
+export default UpcomingCars;
