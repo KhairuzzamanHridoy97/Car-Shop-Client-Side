9b422482ff74aaae3480a250756cab73f7e81278
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-14 20:54:49 +0600

    set upComCar in upcomingCar

---
+++ src/pages/Home/UpcomingCar/UpComCar/UpComCar.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardMedia, Grid } from '@mui/material';
+
+const UpComCar = ({offer}) => {
+    const {name,img}= offer;
+    return (
+        <Grid   item xs={4} sm={4} md={4}>
+        
+        <Card className="latestOffer-Card"  sx={{ minWidth: 275 }}>
+            <CardMedia
+              component="img"
+              
+              style={{ width: 'auto', height: '150px',margin:'0 auto'}}
+              image={img}
+        />
+        <CardContent>
+          <Typography variant="h4" component="div">
+            {name}
+          </Typography>        
+             <button  className="btn btn-outline-dark m-2" >Details</button>             
+        </CardContent>
+          </Card>
+         </Grid>
+    );
+};
+
+export default UpComCar;
--- src/pages/Home/UpcomingCar/UpcomingCars/UpcomingCars.js
+++ src/pages/Home/UpcomingCar/UpcomingCars/UpcomingCars.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Divider from '@mui/material/Divider';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import offer1 from '../../../../images/latestOffer/offer1.jpg'
+import offer2 from '../../../../images/latestOffer/offer2.jpg'
+import offer3 from '../../../../images/latestOffer/offer3.jpg'
+import offer4 from '../../../../images/latestOffer/offer4.jpg'
+import offer5 from '../../../../images/latestOffer/offer5.jpg'
+import offer6 from '../../../../images/latestOffer/offer6.jpg'
+import { Card, Col, Row } from 'react-bootstrap';
+import UpComCar from '../UpComCar/UpComCar';
 
-const UpcomingCars = () => {
+
+const offers =[
+    {
+        "name":"Toyota",
+        "img":offer1
+    },
+    {
+        "name":"Ferrai",
+        "img":offer2
+    },
+    {
+        "name":"Corolla",
+        "img":offer3
+    },
+    {
+        "name":"Alion",
+        "img":offer4
+    },
+    {
+        "name":"Buggati",
+        "img":offer5
+    },
+    {
+        "name":"Mahindra",
+        "img":offer6
+    },
+]
+
+const UpComingCars = () => {
+
     return (
-        <div>
-            <h2 className='text-primary my-5'>Upcoming Cars</h2>
-        </div>
+ 
+            <Container>
+                <Divider />
+                <Box sx={{ flexGrow: 1 }}>
+            <Container>
+                <Typography variant='h3' sx={{fontWeight:500,my:5,color:'success.main'}} component="div">
+                    Our Latest Offers
+                </Typography>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+         {
+             offers.map(offer=><UpComCar
+             offer={offer}
+             key={offer.name}
+             >
+
+             </UpComCar>)
+         }
+        </Grid>
+        </Container>
+      </Box>
+            </Container>
     );
 };
 
-export default UpcomingCars;
+export default UpComingCars;
