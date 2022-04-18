5362e89328192f557b5ced7a9fd53f0d510b5771
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-18 23:51:21 +0600

    set handleDelete in myOrders

--- src/pages/Dashboard/MyOrders/MyOrders.js
+++ src/pages/Dashboard/MyOrders/MyOrders.js
@@ -15,7 +15,7 @@ const MyOrders =()=>{
 
 
     useEffect(()=>{
-        const url=`https://pumpkin-crisp-13332.herokuapp.com/orders?email=${user.email}`
+        const url=`http://localhost:5000/orders?email=${user.email}`
         fetch(url)
         .then(res=>res.json())
         .then(data=>setMyOrders(data))
@@ -26,7 +26,7 @@ const MyOrders =()=>{
         console.log(id);
         const proceed = window.confirm("Are you sure, you want to delete?");
         if (proceed) {
-            fetch(`https://pumpkin-crisp-13332.herokuapp.com/deleteOrder/${id}`, {
+            fetch(`http://localhost:5000/deleteOrder/${id}`, {
                 method: 'DELETE'
             })
                 .then(res => res.json())
