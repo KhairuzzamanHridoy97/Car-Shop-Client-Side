00d9a0296d5a0e0fb3715a5781046d5a4e0f1918
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-18 23:47:45 +0600

    set footer

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,29 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
 
 const Footer = () => {
     return (
-        <div className='mt-5 bg-dark py-3 text-danger'>
-            <h3>This is Footer </h3>
-        </div>
+        <Grid container spacing={3} className='bg-dark text-danger mt-3'>
+        <Grid item xs>
+            <p>
+              Car Shop
+            </p>
+            We are providing best Bike in this town.
+        </Grid>
+        <Grid item xs={6}>
+                 <p>Home</p>
+                <p>Service</p>
+                <p>Contact</p>
+                <p>About BikeShop</p>
+        </Grid>
+        <Grid item xs>
+        <p>Copyright © 2022 KH IT-Tech</p>
+            <p>We promise to our clients and is <br /> constantlystriving to improve  <br />performance and heightened satisfaction of clients and developer.</p>
+        </Grid>
+      </Grid>
+        
     );
 };
 
